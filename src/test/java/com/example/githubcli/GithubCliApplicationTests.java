f29c16fad1d8f7316dda00fe9b7bb3c7d2a78949
MohamedAtef9211 <100563509+MohamedAtef9211@users.noreply.github.com>
2023-02-27 13:13:12 +0200

    Add files via upload

---
+++ src/test/java/com/example/githubcli/GithubCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubcli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
